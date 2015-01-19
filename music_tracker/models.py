0e617955ecce29cdd572301bcef7b2e7b48bd1c4
Eric Fulmer <EFulmer@gmail.com>
2015-01-19 12:50:06 -0500

    we now have actual models...

--- music_tracker/models.py
+++ music_tracker/models.py
@@ -1,19 +1,29 @@
 # -*- coding: utf-8 -*-
 # music_tracker/models.py
 
-from .music_tracker import db
+from . import db
 
 
-class ArtistItem(db.Model):
+class ArtistInfo(db.Model):
+    """Info about a musical artist."""
+    id      = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name    = db.Column(db.String(128))
+    album   = db.Column(db.String(128))
+    track   = db.Column(db.String(128))
+    bio     = db.Column(db.String(1024))
+    yt      = db.Column(db.String(128))
+    spotify = db.Column(db.String(128))
+    lastfm  = db.Column(db.String(128))
+    rym     = db.Column(db.String(128))
 
-    # Columns
 
+class UsersArtist(db.Model):
+    id         = db.Column(db.ForeignKey('user.id'))
+    artist     = db.Column(db.ForeignKey('artistInfo.id'))
+    date_added = db.Column(db.DateTime)
+    active     = db.Column(db.Boolean)
+
+
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-
-    # user will be used later
-    # user = db.Column(db.String(128))
-
-    artist = db.Column(db.String(128))
-
-    album = db.Column(db.String(128))
-
+    # TODO login stuff ...
