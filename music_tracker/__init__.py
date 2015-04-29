90684987d8f30be8f6a808b9a35b439c1ebe8a02
Eric Fulmer <EFulmer@gmail.com>
2015-04-28 21:12:14 -0400

    more config updates

--- music_tracker/__init__.py
+++ music_tracker/__init__.py
@@ -1,5 +1,4 @@
-# music_tracker/__init__.py
-# -*- coding: utf-8 -*-
+# music_tracker/__init__.py # -*- coding: utf-8 -*-
 import os
 
 from flask import Flask
@@ -11,6 +10,12 @@ from flask.ext.mail import Mail
 if os.environ.get('HEROKU'):
     app = Flask(__name__)
     app.config.from_pyfile('../config.py')
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+    app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
+    app.config['MAIL_PORT'] = os.environ.get('MAIL_PORT')
+    app.config['MAIL_USE_SSL'] = os.environ.get('MAIL_USE_SSL')
+    app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+    app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 else:
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object('config')
